4e5361b4fe0f078d479be7421051f811a9470d03
Hawk Ticehurst <hawkticehurst@gmail.com>
2025-01-27 17:55:59 -0800

    Revert private property field declarations

--- src/App.ts
+++ src/App.ts
@@ -6,10 +6,10 @@ const pick = (dict: string[]) => dict[Math.round(Math.random() * 1000) % dict.le
 const label = () => `${pick(adjectives)} ${pick(colours)} ${pick(nouns)}`;
 
 class WordList extends HTMLElement {
-	private _tbody = this.querySelector('tbody');
-	private _rowTemplate = this.querySelector('template');
-	private _rowId = 1;
-	private _selectedRow: HTMLTableRowElement | null = null;
+	tbody = this.querySelector('tbody');
+	rowTemplate = this.querySelector('template');
+	rowId = 1;
+	selectedRow: HTMLTableRowElement | null = null;
 
 	constructor() {
 		super();
@@ -39,8 +39,8 @@ class WordList extends HTMLElement {
 				return;
 			}
 			case 'UPDATE_10th_ROW': {
-				if (this._tbody) {
-					const rows = this._tbody.children;
+				if (this.tbody) {
+					const rows = this.tbody.children;
 					for (let i = 0; i < rows.length; i += 10) {
 					const label = rows[i].querySelector('td:nth-child(2) > a');
 					if (label && label.firstChild) {
@@ -54,19 +54,19 @@ class WordList extends HTMLElement {
 				event.preventDefault();
 				const row = target.closest('tr');
 				if (row) {
-          if (this._selectedRow) {
-            this._selectedRow.classList.remove('danger');
+          if (this.selectedRow) {
+            this.selectedRow.classList.remove('danger');
           }
-          this._selectedRow = row;
-          this._selectedRow.classList.add('danger');
+          this.selectedRow = row;
+          this.selectedRow.classList.add('danger');
         }
         return;
 			}
 			case 'REMOVE_ROW': {
 				const row = target.closest('tr');
 				if (row) {
-					if (row === this._selectedRow) {
-						this._selectedRow = null;
+					if (row === this.selectedRow) {
+						this.selectedRow = null;
 					}
 					row.remove();
 				}
@@ -84,34 +84,34 @@ class WordList extends HTMLElement {
 	}
 
 	addRows(n = 1000) {
-		if (!this._tbody || !this._rowTemplate) return;
+		if (!this.tbody || !this.rowTemplate) return;
     const fragment = document.createDocumentFragment();
 
     while (n > 0) {
-      const tr = this._rowTemplate.content.cloneNode(true) as HTMLTableRowElement;
+      const tr = this.rowTemplate.content.cloneNode(true) as HTMLTableRowElement;
       const td1 = tr.querySelector('td:nth-child(1)');
-      if (td1) td1.textContent = String(this._rowId++);
+      if (td1) td1.textContent = String(this.rowId++);
       const td2a = tr.querySelector('td:nth-child(2) a');
       if (td2a) td2a.textContent = label();
       fragment.appendChild(tr);
       n -= 1;
     }
 
-    this._tbody.appendChild(fragment);
+    this.tbody.appendChild(fragment);
 	}
 
 	clearRows() {
-		if (!this._tbody) return;
-    this._tbody.textContent = '';
-    this._selectedRow = null;
+		if (!this.tbody) return;
+    this.tbody.textContent = '';
+    this.selectedRow = null;
 	}
 
 	swapRows() {
-		if (!this._tbody || this._tbody.children.length < 2) return;
-		const secondRow = this._tbody.children[1];
-		const secondToLastRow = this._tbody.children[this._tbody.children.length - 2];
-		this._tbody.insertBefore(secondToLastRow, secondRow);
-		this._tbody.insertBefore(secondRow, this._tbody.children[this._tbody.children.length - 1]);
+		if (!this.tbody || this.tbody.children.length < 2) return;
+		const secondRow = this.tbody.children[1];
+		const secondToLastRow = this.tbody.children[this.tbody.children.length - 2];
+		this.tbody.insertBefore(secondToLastRow, secondRow);
+		this.tbody.insertBefore(secondRow, this.tbody.children[this.tbody.children.length - 1]);
   }
 }
 
